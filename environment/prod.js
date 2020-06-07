3ca36e1ca9cb4fdf9eafc1b8e8d2a3127f90b248
borisbouju <boris.bouju@gmail.com>
2020-06-07 03:39:52 +0200

    idem

--- environment/prod.js
+++ environment/prod.js
@@ -3,5 +3,7 @@ const path = require('path')
 module.exports = {
     dbUrl : 'mongodb+srv://admin:admin@cluster0-zptsp.gcp.mongodb.net/twitter?retryWrites=true&w=majority',
     cert : path.join(__dirname, ''),
-    key : path.join(__dirname, '')
+    key : path.join(__dirname, ''),
+    httpPort : 80,
+    httpsPort : 443
 }
